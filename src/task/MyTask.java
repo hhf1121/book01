bad49a612783423c539940d4e172afab1dfcca4f
hhf <290071186@qq.com>
2020-08-02 15:26:22 +0800

    集成elastic-job,实现简单job

---
+++ src/task/MyTask.java
@@ -0,0 +1,37 @@
+package task;
+
+import com.dangdang.ddframe.job.api.ShardingContext;
+import com.dangdang.ddframe.job.api.simple.SimpleJob;
+import com.google.common.collect.Lists;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * 简单任务
+ */
+public class MyTask implements SimpleJob {
+
+    List<Boolean> lists= Lists.newArrayList(false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false);
+
+    @Override
+    public void execute(ShardingContext shardingContext) {
+        System.out.println("分片:"+shardingContext.getShardingItem());
+        System.out.println("----------执行----------"+ LocalDateTime.now());
+        List<Boolean> result=Lists.newArrayList();
+        for (Boolean list : lists) {
+            System.out.println("源数据:"+lists);
+            if(result.size()==lists.size()){
+                break;
+            }
+            list=true;
+            result.add(list);
+            System.out.println("结果："+result);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+
+            }
+        }
+    }
+}
---
+++ src/task/RunTask.java
@@ -0,0 +1,42 @@
+package task;
+
+import com.dangdang.ddframe.job.config.JobCoreConfiguration;
+import com.dangdang.ddframe.job.config.simple.SimpleJobConfiguration;
+import com.dangdang.ddframe.job.lite.api.JobScheduler;
+import com.dangdang.ddframe.job.lite.config.LiteJobConfiguration;
+import com.dangdang.ddframe.job.reg.base.CoordinatorRegistryCenter;
+import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperConfiguration;
+import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
+
+public class RunTask {
+
+    //创建并初始化zk链接
+    public static CoordinatorRegistryCenter initZK(){
+        //创建zk链接
+        ZookeeperConfiguration zookeeperConfiguration=new ZookeeperConfiguration("127.0.0.1:2181","elastic-job-example-java");
+        //超时时间
+        zookeeperConfiguration.setSessionTimeoutMilliseconds(500);
+        //创建注册中心
+        CoordinatorRegistryCenter zookeeperRegistryCenter = new ZookeeperRegistryCenter(zookeeperConfiguration);
+        //初始化
+        zookeeperRegistryCenter.init();
+        return zookeeperRegistryCenter;
+    }
+
+
+    public static void startTask(CoordinatorRegistryCenter zookeeperRegistryCenter){
+        //创建：JobCoreConfiguration       任务名，调度表达式，分片数量
+        JobCoreConfiguration.Builder jobCoreConfiguration = JobCoreConfiguration.newBuilder("my-simplejob", "0/3 * * * * ?", 3);
+        //创建：SimpleJobConfiguration      任务配置，任务执行类的完全限定名
+        SimpleJobConfiguration simpleJobConfiguration = new SimpleJobConfiguration(jobCoreConfiguration.build(), MyTask.class.getCanonicalName());
+        //启动任务
+        new JobScheduler(zookeeperRegistryCenter, LiteJobConfiguration.newBuilder(simpleJobConfiguration).overwrite(true).build()).init();
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        CoordinatorRegistryCenter coordinatorRegistryCenter = initZK();
+//        Thread.sleep(100000);
+        startTask(coordinatorRegistryCenter);
+    }
+}
